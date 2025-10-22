740071b08c4cafc4cabfa2745064f6c5da34c2e0
smejiaa19 <silviomejiagarcia@gmail.com>
2025-10-21 18:05:31 -0600

    Se me habia olvidado que habia mas :v

--- src/main/java/run/main.java
+++ src/main/java/run/main.java
@@ -4,6 +4,8 @@ import entities.Autor;
 import services.dao.MyDao;
 import services.interfaces.ICRUD;
 
+import java.util.List;
+
 public class main {
     public static final ICRUD dao = new MyDao();
 
@@ -17,5 +19,33 @@ public class main {
         r.setNombre("Ruben Dario");
         r.setNacionalidad("Nicaraguense");
         dao.insert(r);
+    }
+
+    public static void listarAutores(){
+        System.out.println("Registro Almacenados: ");
+        List<Autor> autores = dao.getAll("autores.All", Autor.class);
+        autores.forEach(autor -> System.out.println(autor.getNombre()));
+    }
+
+    public static void editarAutor(){
+        Autor a = new Autor();
+        a = dao.findById(1, Autor.class);
+        a.setNacionalidad("Colombiana");
+        dao.update(a);
+    }
+
+    public static void eliminarAutor(){
+        Autor a = new Autor();
+        a = dao.findById(2, Autor.class);
+        dao.delete(a);
+    }
+
+    public static void main(String[] args){
+        insertarAutor();
+        listarAutores();
+        editarAutor();
+        listarAutores();
+        eliminarAutor();
+        listarAutores();
     }
 }
